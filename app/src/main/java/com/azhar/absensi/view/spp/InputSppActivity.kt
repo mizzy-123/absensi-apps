4ea22b376d6301b94b7877789c99d8b56bad56fd
colondum <decarabian21@gmail.com>
2024-05-10 17:40:49 +0700

    add: 2 activity, and add: binding

---
+++ app/src/main/java/com/azhar/absensi/view/spp/InputSppActivity.kt
@@ -0,0 +1,21 @@
+package com.azhar.absensi.view.spp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.azhar.absensi.R
+
+class InputSppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_input_spp)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
