428fe9ef827fff4ecaacbda362bd7f9ce20114d4
colondum <decarabian21@gmail.com>
2024-05-11 10:24:33 +0700

    add: menu pembayaran spp

---
+++ app/src/main/java/com/azhar/absensi/view/spp/MainSppActivity.kt
@@ -0,0 +1,34 @@
+package com.azhar.absensi.view.spp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.azhar.absensi.R
+import com.azhar.absensi.databinding.ActivityMainSppBinding
+
+class MainSppActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainSppBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainSppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
+    }
+
+    private fun setupAction(){
+        binding.cvInputSpp.setOnClickListener {
+            val intent = Intent(this@MainSppActivity, InputSppActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cvRiwayatSpp.setOnClickListener {
+            val intent = Intent(this@MainSppActivity, RiwayatSppActivity::class.java)
+        }
+    }
+}
